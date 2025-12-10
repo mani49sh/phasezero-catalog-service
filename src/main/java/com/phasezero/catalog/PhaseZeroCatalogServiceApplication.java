f30f5f0e9561e2d5cbba1f9894bc3baf19bce8ba
Monish Roy <monishroy1998@gmail.com>
2025-12-10 20:50:11 +0530

    Project setup

---
+++ src/main/java/com/phasezero/catalog/PhaseZeroCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phasezero.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseZeroCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseZeroCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phasezero/catalog/PhaseZeroCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phasezero.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseZeroCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
